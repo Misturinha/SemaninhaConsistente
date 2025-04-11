07b013cbb2fb6ec8592657b07f266aa4a29fc46c
LucasSantanaCWB <lucas@sisand.com.br>
2025-04-11 00:02:24 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using HttpClient httpClient = new HttpClient();
+try
+{
+    // Make GET request
+    HttpResponseMessage response = await httpClient.GetAsync("https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=palio_do_mal&api_key=681b18bb53abc9da1ba5d84252cd6aac&format=json");
+
+    // Ensure success status code
+    response.EnsureSuccessStatusCode();
+
+    // Read response content
+    string responseBody = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine("Response:");
+    Console.WriteLine(responseBody);
+}
+catch (Exception)
+{
+
+	throw;
+}
+Console.WriteLine("Hello, World!");
